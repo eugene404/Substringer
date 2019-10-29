537235e03111e99bcddf2b81bc644b67bfc0ca4d
Eugene <eugenesny@icloud.com>
2019-10-29 22:13:09 +0200

    1

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -12,22 +12,20 @@ namespace ConsoleApp15
         {
             int l = a.Length;
 
-            string word = a.Substring(1,l-1);
+            string word = a.Substring(1,l-2);
         
             return word;
         }
+
         static void Main(string[] args)
         {
             Substringer s = new Substringer();
 
-            string name = "Eugene";
-            name.Substring(0);
-            //char[] list = name.ToCharArray();
-
-            Console.WriteLine("nameLimiter method " +  );
+            string r = s.nameLimiter("Eugene");
+            
+            Console.WriteLine(r);
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
